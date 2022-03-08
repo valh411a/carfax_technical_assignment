7b043e9f91cdb4fddcc43ab5cc0e4c531955d60c
Cameron DeVaul <camerondev343@gmail.com>
2022-03-08 14:28:24 -0500

    Migrated to Jetpack Compose for flexible card creation

--- app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
+++ app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
@@ -5,6 +5,19 @@ import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.appcompattheme.AppCompatTheme
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -32,6 +45,26 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
+        val card = findViewById<ComposeView>(R.id.cardView)
+        card.setContent {
+            AppCompatTheme {
+                Greeting()
+            }
+        }
+
     }
+}
+
+@Preview
+@Composable
+private fun Greeting() {
+    Text(
+        text = stringResource(R.string.app_name),
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
 
