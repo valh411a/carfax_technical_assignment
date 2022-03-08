7e16d53b8dfe7e1bcd7c38ebfd5835844a352dce
Cameron DeVaul <camerondev343@gmail.com>
2022-03-08 13:04:35 -0500

    Added Firebase Realtime Database for data storage and usage

--- app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
+++ app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.carfaxtechnicalassignment
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val database = Firebase.database
+        Firebase.database.setPersistenceEnabled(true)
+        val myRef = database.getReference("message")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 }
+
