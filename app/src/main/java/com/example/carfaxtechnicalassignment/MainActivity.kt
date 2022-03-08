672ad496dca6383a31fb1aa8bbb354ee282dd1ae
Cameron DeVaul <camerondev343@gmail.com>
2022-03-08 16:03:41 -0500

    Formatted Compose for Architecture Layout

--- app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
+++ app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.carfaxtechnicalassignment
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,13 +12,13 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.accompanist.appcompattheme.AppCompatTheme
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -25,13 +26,24 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
-    private val database = Firebase.database
+private val database = Firebase.database
 
+class FireBaseStarter : android.app.Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        database.setPersistenceEnabled(true)
+    }
+
+}
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Firebase.database.setPersistenceEnabled(true)
+
+        setContent {
+            Screen()
+        }
         val myRef = database.getReference("message")
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -45,21 +57,38 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
-        val card = findViewById<ComposeView>(R.id.cardView)
-        card.setContent {
-            AppCompatTheme {
-                Greeting()
-            }
-        }
+
 
     }
 }
 
-@Preview
+
 @Composable
-private fun Greeting() {
+fun Screen(viewModel: ScreenViewModel = viewModel()) {
+    when (val uiState = viewModel.uiState.value) {
+        ScreenState.Success -> Greeting(text = uiState.toString())
+        ScreenState.Error -> ErrorOccured()
+    }
+
+
+}
+
+class ScreenViewModel : ViewModel() {
+    private val _uiState = mutableStateOf<ScreenState>(ScreenState.Success)
+    val uiState: State<ScreenState>
+        get() = _uiState
+}
+
+sealed class ScreenState {
+    object Success : ScreenState()
+    object Error : ScreenState()
+
+}
+
+@Composable
+private fun Greeting(text: String) {
     Text(
-        text = stringResource(R.string.app_name),
+        text = text,
         style = MaterialTheme.typography.h5,
         modifier = Modifier
             .fillMaxWidth()
@@ -68,3 +97,14 @@ private fun Greeting() {
     )
 }
 
+@Composable
+private fun ErrorOccured() {
+    Text(
+        text = "An error has occured when attempting to load the list.",
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
