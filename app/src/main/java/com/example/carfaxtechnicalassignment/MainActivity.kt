605af40e685ddadb05da74306d8ff3e1d33143c8
Cameron DeVaul <camerondev343@gmail.com>
2022-03-09 18:29:38 -0500

    Added documentation, fixed spelling errors, cleaned up imports

--- app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
+++ app/src/main/java/com/example/carfaxtechnicalassignment/MainActivity.kt
@@ -22,6 +22,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -33,6 +35,10 @@ import java.io.Serializable
 import java.text.NumberFormat
 import java.util.*
 
+/**
+ * Preface class to run on app launch that sets the Firebase persistence to "true" so that
+ * data can be accessed while offline.
+ */
 class FireBaseStarter : android.app.Application() {
     override fun onCreate() {
         super.onCreate()
@@ -44,6 +50,9 @@ class FireBaseStarter : android.app.Application() {
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Overridden onCreate method, standard procedure for Activity Lifecycle control
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -66,6 +75,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * pulls the needed info for the compose methods and data inclusion from Firebase
+     * @param CarDataList the Firebase RTDB location for the information to be pulled.
+     * @return parsedInventory the parsed ArrayList of info that was gathered.
+     */
     private fun getCarList(
         CarDataList: ArrayList<*>
     ): ArrayList<CarInfoModel> {
@@ -86,6 +100,7 @@ class MainActivity : AppCompatActivity() {
             format.currency = Currency.getInstance("USD")
             val price = format.format(hashMap["currentPrice"].toString().toDouble())
 
+            //TODO: format mileage to be "##.#k mi"
             val mileage = hashMap["mileage"].toString()
 
             //trying to create a special extraction rule from the firebase data took too long so I
@@ -137,8 +152,11 @@ class MainActivity : AppCompatActivity() {
         return parsedInventory
     }
 
+    /**
+     * data class helper to make changes easier to track in the Compose methods
+     */
     data class CarInfoModel(
-        val year: String? = null,
+        val year: String,
         val make: String,
         val model: String,
         val trim: String,
@@ -154,11 +172,15 @@ class MainActivity : AppCompatActivity() {
         val fuel: String,
         val phoneNum: String,
         val imageUrl: String
-    ) : Serializable {
-        //TODO: Add variable to handle image links
+    ) : Serializable
 
-    }
-
+    /**
+     * Main Jetpack Compose method for generating the screen's resources, and Displaying them with
+     * saved states through Architecture component helpers (see below)
+     *
+     * @param inventoryList the generated list of cars for the cards to be created from
+     * @param viewModel the viewModel generated from the ScreenViewModel class below
+     */
     @Composable
     fun Screen(inventoryList: ArrayList<*>, viewModel: ScreenViewModel = viewModel()) {
         if (inventoryList.size > 1) {
@@ -166,7 +188,8 @@ class MainActivity : AppCompatActivity() {
                 ScreenState.Success -> {
                     LazyColumn {
                         items(inventoryList) { cardInfoList ->
-                            ListingCard(cardInfoList = cardInfoList)
+                            //TODO Switch back to ListingCard once button functionality is complete
+                            Details(cardInfoList = cardInfoList)
                         }
                     }
                 }
@@ -179,22 +202,39 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Architecture component helper for the Screen function, generates the ViewModel on a
+     * successful data gathering attempt
+     *
+     */
     class ScreenViewModel : ViewModel() {
         private val _uiState = mutableStateOf<ScreenState>(ScreenState.Success)
         val uiState: State<ScreenState>
             get() = _uiState
     }
 
+    /**
+     * sealed class for lifecycle monitoring using screen states
+     */
     sealed class ScreenState {
         object Success : ScreenState()
         object Error : ScreenState()
 
     }
 
+    /**
+     * Jetpack Compose function that draws the elements that make up the default homepage.
+     * TODO: Button design is not yet implemented, so this function is currently 'disabled' in lieu
+     * of the more detailed 'details' page, which contains all of the listed info as well as the
+     * additional info as expected from the assignment's requirements.
+     *
+     * @param cardInfoList the list of info to use to fill out the cards with
+     * @optIn uses an experimental Coil API from Compose to grab the image directly from a given link
+     */
     @OptIn(ExperimentalCoilApi::class)
     @Composable
     private fun ListingCard(cardInfoList: Any) {
-        val cardInfo = arrayListOf<CarInfoModel>(cardInfoList as CarInfoModel).elementAt(0)
+        val cardInfo = arrayListOf(cardInfoList as CarInfoModel).elementAt(0)
         Log.i(TAG, "Image Url: " + cardInfo.imageUrl)
         Card(
             modifier = Modifier
@@ -222,7 +262,7 @@ class MainActivity : AppCompatActivity() {
                 )
 
                 Text(
-                    cardInfo.year.toString() + " " +
+                    cardInfo.year + " " +
                             cardInfo.make + " " +
                             cardInfo.model
                 )
@@ -260,10 +300,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Fallback "error occurred" compose component which displays if the app failed to successfully
+     * fetch the necessary data from the database
+     *
+     * @see Screen for usage
+     */
     @Composable
     private fun ErrorOccurred() {
         Text(
-            text = "An error has occured when attempting to load the list.",
+            text = "An error has occurred when attempting to load the list.",
             style = MaterialTheme.typography.h5,
             modifier = Modifier
                 .fillMaxWidth()
@@ -272,64 +318,148 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    /**
+     * The more exhaustive list of details for each car in the cardInfoList
+     *
+     * This page is displayed by default instead of the ListingCard class until functionality is
+     * implemented for button navigation.
+     *
+     * @see ListingCard for explanation on the @optIn and cardInfoList parameter.
+     * @param cardInfoList
+     */
+    @OptIn(ExperimentalCoilApi::class)
     @Composable
     private fun Details(cardInfoList: Any) {
-        val cardInfo = arrayListOf<CarInfoModel>(cardInfoList as CarInfoModel).elementAt(0)
-        Scaffold {
+        val cardInfo = arrayListOf(cardInfoList as CarInfoModel).elementAt(0)
+        Column {
+            //TODO: Fix image width clipping
+            Image(
+                painter = rememberImagePainter(
+                    cardInfo.imageUrl,
+                    builder = { size(OriginalSize) }),
+                contentDescription = "carImage",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
             Text(
-                cardInfo.year.toString() + " " +
+                cardInfo.year + " " +
                         cardInfo.make + " " +
                         cardInfo.model
             )
             Text(
                 cardInfo.price + "\t|\t" +
-                        cardInfo.mileage
+                        cardInfo.mileage,
+                fontWeight = FontWeight.Bold
             )
-            Divider()
-            Text(text = "Vehicle Info")
+            Divider(modifier = Modifier.padding(16.dp))
+            Text(
+                text = "Vehicle Info",
+                modifier = Modifier
+                    .padding(start = 16.dp, bottom = 16.dp)
+                    .width(100.dp),
+                fontWeight = FontWeight.Bold
+            )
             Row {
-                Text(text = "Location")
-                Text(cardInfo.location)
+                Text(
+                    text = "Location\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
+                Text(
+                    cardInfo.location,
+                    textAlign = TextAlign.End
+                )
             }
             Row {
-                Text(text = "Exterior Color")
+                Text(
+                    text = "Exterior Color\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(cardInfo.exColor)
             }
             Row {
-                Text(text = "Interior Color")
+                Text(
+                    text = "Interior Color\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(cardInfo.inColor)
             }
             Row {
-                Text(text = "Drive Type")
+                Text(
+                    text = "Drive Type\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(text = cardInfo.driveType)
             }
             Row {
-                Text(text = "Transmission")
+                Text(
+                    text = "Transmission\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(text = cardInfo.trans)
             }
             Row {
-                Text(text = "Body Style")
+                Text(
+                    text = "Body Style\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(text = cardInfo.bodyStyle)
             }
             Row {
-                Text(text = "Engine")
+                Text(
+                    text = "Engine\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(text = cardInfo.engine)
             }
             Row {
-                Text(text = "Fuel")
+                Text(
+                    text = "Fuel\t",
+                    modifier = Modifier
+                        .padding(start = 16.dp, bottom = 16.dp)
+                        .width(150.dp),
+                    color = Color.Gray
+                )
                 Text(text = cardInfo.fuel)
             }
+            //TODO: force "call dealer" to bottom of screen
             BottomAppBar {
-                Button(onClick = {
-                    Toast.makeText(
-                        applicationContext,
-                        "Implement Call Dealer button switch to phone app with number input.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }) {
+                Button(
+                    onClick = {
+                        Toast.makeText(
+                            applicationContext,
+                            "TODO: Implement Call Dealer button switch to phone app with number input.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    elevation = elevation(0.dp, 0.dp)
+                ) {
                     Text(text = "Call Dealer")
                 }
             }
+            Divider(Modifier.padding(8.dp))
         }
     }
 
